5d4dcc7121b70244cb1d0d55434f5a12a9ee6efb
Roman Overko <roman.overko@iota.org>
2026-02-22 14:49:17 +0100

    chore: update deps
    Signed-off-by: Roman Overko <roman.overko@iota.org>

--- lua/plugins/mdx.lua
+++ lua/plugins/mdx.lua
@@ -1,5 +1,23 @@
+-- return {
+--   "davidmh/mdx.nvim",
+--   config = true,
+--   dependencies = { "nvim-treesitter/nvim-treesitter" },
+-- }
 return {
   "davidmh/mdx.nvim",
-  config = true,
   dependencies = { "nvim-treesitter/nvim-treesitter" },
 }
+-- return {
+--   {
+--     "jose-elias-alvarez/mdx.nvim",
+--     config = function()
+--       require("mdx").setup({
+--         -- Your configuration options here
+--         -- Example:
+--         -- enable = true,
+--         -- syntax = "markdown",
+--       })
+--     end,
+--     dependencies = { "nvim-treesitter/nvim-treesitter" },
+--   },
+-- }
